66a4d8099181a5432ac875a83cc6e6abfdb7f384
Fred-Abongo <ngoloabongo32@gmail.com>
2023-07-25 17:53:58 -0700

    all the changes to be commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,3 +11,7 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> a9181dca588cb76227015c5e608acc5de63fbe65
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 this is a readme
+=======
+Hello World
+>>>>>>> a9181dca588cb76227015c5e608acc5de63fbe65
