7b6de07125b1339c784597a1ad3961950ad5f0f0
Fred-Abongo <ngoloabongo32@gmail.com>
2023-07-31 01:59:17 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - main function
- *
- * Return: 0 when successful
- */
+ *  * main - main function
+ *   *
+ *    * Return: 0 when successful
+ *     */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
-return (0);
+	    printf("with proper grammar, but the outcome is a piece of art,\n");
+	        return (0);
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> a9181dca588cb76227015c5e608acc5de63fbe65
