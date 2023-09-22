011371db44d321355f2857dcea558326fca26633
Fred-Abongo <ngoloabongo32@gmail.com>
2023-09-22 07:26:49 -0700

    update alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints the lowercase alphabet from 'a' to 'z'.
+ *
+ * Description: This function prints all the lowercase letters of the
+ * alphabet in ascending order, followed by a newline character.
+ */
+void print_alphabet(void)
+{
+	char letter;
 
-	/**
-	 * print_alphabet - print all the alphabet in lowercase
-	 */
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
-	}
+	_putchar('\n');
+}
